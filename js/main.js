3864b33770767014d42e3f6870e5a994f0d5e946
minju0726 <minju0726@gmail.com>
2024-02-06 11:49:56 +0900

    html , css 수정

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $(".mainmenu > li").mouseenter(function(){
+//         $(this).fin(".submenu").stop().slideDown();
+//     });
+    
+//     $(".mainmenu > li").mouseleave(function(){
+//         $(this).fin(".submenu").stop().slideUp();
+//     });
+// })
